8b561f6914492188d7569f11a29b5963a1ebefa4
Abrifq <melezorus34@gmail.com>
2024-08-21 22:15:03 +0300

    Renaming, adding basic output code

--- solution.c
+++ solution.c
@@ -2,42 +2,55 @@
 
 double calculateNextPrice();
 double calculateDelta();
+int doCalculations();
+//definitions: arrayName__index1Name_index2Name_..._indexNName
 
 int main(int argc, char** argv) {
 
     int n, T, c;
     scanf(" %d %d %d", &n, &T, &c);
 
-    double* P = malloc(sizeof(double) * n);
-    double* L = malloc(sizeof(double) * n);
-    for (int i = 0; i < n;i++) scanf(" %lf %lf", &P[i], &L[i]);
+    double* BuyPrice__buyer = malloc(sizeof(double) * n);
+    double* MaxChocolateWeCanSell__buyer = malloc(sizeof(double) * n);
+    for (int i = 0; i < n;i++) scanf(" %lf %lf", &BuyPrice__buyer[i], &MaxChocolateWeCanSell__buyer[i]);
 
-    double** k = malloc(sizeof(double*) * n);
+    double** CoefficentOfMarketInfluence__buyer_day = malloc(sizeof(double*) * n);
     for (int i = 0; i < n; i++) {
-        k[i] = malloc(sizeof(double) * T);
+        CoefficentOfMarketInfluence__buyer_day[i] = malloc(sizeof(double) * T);
         for (int i2 = 0; i2 < T;i2++)
-            scanf(" %lf", &k[i][i2]);
+            scanf(" %lf", &CoefficentOfMarketInfluence__buyer_day[i][i2]);
     }
-    double** noise = malloc(sizeof(double*) * n);
+    double** RandomNoise__buyer_day = malloc(sizeof(double*) * n);
     for (int i = 0; i < n; i++) {
-        noise[i] = malloc(sizeof(double) * T);
+        RandomNoise__buyer_day[i] = malloc(sizeof(double) * T);
         for (int i2 = 0; i2 < T;i2++)
-            scanf(" %lf", &noise[i][i2]);
+            scanf(" %lf", &RandomNoise__buyer_day[i][i2]);
     }
-    double*** alpha = malloc(sizeof(double**) * (n)); //they like making things complicated, don't they? First "block" is "0 lines" since it has i-1 lines
+
+    //they like making things complicated, don't they? First "block" is "0 lines" since it has i-1 lines
+    double*** CoefficientOfBuyerInfluence__fromBuyer_toBuyer_day = malloc(sizeof(double**) * (n));
     for (int i = 0; i < n; i++) {
-        if (i == 0) { alpha[i] = NULL; continue; }
-        alpha[i] = malloc(sizeof(double**) * (i - 1));
+        if (i == 0) { CoefficientOfBuyerInfluence__fromBuyer_toBuyer_day[i] = NULL; continue; }
+        CoefficientOfBuyerInfluence__fromBuyer_toBuyer_day[i] = malloc(sizeof(double**) * (i - 1));
         for (int i2 = 0; i2 < i - 1;i2++) {
-            alpha[i][i2] = malloc(sizeof(double*) * T);
+            CoefficientOfBuyerInfluence__fromBuyer_toBuyer_day[i][i2] = malloc(sizeof(double*) * T);
             for (int i3 = 0; i3 < T;i3++)
-                scanf(" %lf", &alpha[i][i2][i3]);
+                scanf(" %lf", &CoefficientOfBuyerInfluence__fromBuyer_toBuyer_day[i][i2][i3]);
         }
     }
-    double* beta = malloc(sizeof(double) * T);
-    for (int i = 0; i < T;i++) scanf(" %lf", &beta[i]);
+    double* CoefficentOfNDaysAgo = malloc(sizeof(double) * T);
+    for (int i = 0; i < T;i++) scanf(" %lf", &CoefficentOfNDaysAgo[i]);
 
     //do the stuff
-    //output
+    int** ChocolatesSold__buyer = malloc(sizeof(int*) * T);
+    for (int i = 0;i < T;i++) {
+        ChocolatesSold__buyer[i] = malloc(sizeof(int) * n);
+        for (int i2 = 0;i2 < n;i2++) ChocolatesSold__buyer[i][i2] = doCalculations();
+    }
 
+    //output
+    for (int i = 0; i < T;i++) {
+        for (int i2 = 0;i < n;i++) { if (i2 != 0)putchar(' ');printf("%d", ChocolatesSold__buyer[i][i2]); }
+        putchar('\n');
+    }
 }
